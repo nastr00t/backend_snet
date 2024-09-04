77c2ba6a4ba699247122154ea25fbbf9178dc843
nastr00t <elvis.acosta@gmail.com>
2024-09-04 06:29:51 -0500

    pra despliegue

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import cors from "cors";
 import UserRoutes from "./routes/user.js"
 import PublicationRoutes from "./routes/publication.js"
 import FollowRoutes from "./routes/follow.js"
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 
 // Mensaje de bienvenida para verificar que ejecutó bien la API de Node
@@ -21,7 +24,9 @@ const puerto = process.env.PORT || 3900;
 // Configurar cors para hacer las peticiones correctamente
 app.use(cors({
     origin: '*',
-    methods: 'GET,HEAD, PUT,PATCH,POST,DELETE'
+    methods: 'GET,HEAD, PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionSuccessStatus:204
 }));
 
 // Decodificar los datos desde los formularios para convertirlos en objetos JS
@@ -32,6 +37,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/user', UserRoutes);
 app.use('/api/publication', PublicationRoutes);
 app.use('/api/follow', FollowRoutes);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Configuración para servir archivos estáticos (imágenes de avatar)
+app.use('/uploads/avatars', express.static(path.join(__dirname, 'uploads', 'avatars')));
+
+// Configuración para servir archivos estáticos (imágenes de publicaciones)
+app.use('/uploads/publications', express.static(path.join(__dirname, 'uploads', 'publications')));
 
 // Configurar el servidor Node
 app.listen(puerto, () => {
